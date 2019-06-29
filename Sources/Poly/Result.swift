aefce54bd2da1019eda604cc8a62ff2f79125720
Mathew Polzin <matt.polzin@gmail.com>
2019-06-29 09:49:16 -0700

    switch to native Swift Result type introduced with Swift 5

--- Sources/Poly/Result.swift
+++ Sources/Poly/Result.swift
@@ -1,46 +1,22 @@
 //
-//  Result.swift
+//  Result+Value.swift
 //  Poly
 //
 //  Created by Mathew Polzin on 12/5/18.
 //
 
-/// Result type internal to this library.
-enum Result<T, E: Swift.Error> {
-	case success(T)
-	case failure(E)
+internal extension Swift.Result {
+    var value: Success? {
+        guard case .success(let val) = self else {
+            return nil
+        }
+        return val
+    }
 
-	var value: T? {
-		guard case .success(let val) = self else {
-			return nil
-		}
-		return val
-	}
-
-	var error: E? {
-		guard case .failure(let err) = self else {
-			return nil
-		}
-		return err
-	}
-
-	func map<U>(_ transform: (T) -> U) -> Result<U, E> {
-		switch self {
-		case .failure(let err):
-			return .failure(err)
-		case .success(let val):
-			return .success(transform(val))
-		}
-	}
-}
-
-extension Result: CustomStringConvertible where T: CustomStringConvertible, E: CustomStringConvertible {
-	var description: String {
-		switch self {
-		case .success(let val):
-			return String(describing: val)
-		case .failure(let err):
-			return String(describing: err)
-		}
-	}
+    var error: Error? {
+        guard case .failure(let err) = self else {
+            return nil
+        }
+        return err
+    }
 }
